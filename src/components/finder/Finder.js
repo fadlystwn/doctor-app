1fd65c360667a66a4aa5bf2213c6b9508db1e8c7
fadly <fadlystwn@gmail.com>
2021-09-07 07:46:57 +0700

    remove unused import

--- src/components/finder/Finder.js
+++ src/components/finder/Finder.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box';
 
